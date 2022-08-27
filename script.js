b81cf67656464afd97c35531238fe7971b7995be
Madhan Kumar <Madhankumar26291@gmail.com>
2022-08-27 17:26:50 +0530

    game

--- script.js
+++ script.js
@@ -1,114 +1,158 @@
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
-
-canvas.width=innerWidth
-canvas.height=innerHeight
-const gravity=0.5
-class Player {
-    constructor(){
-        this.position = {
-            x:100,
-            y:100 
-        }
-        this.velocity= {
-            x:0,
-            y:1
-        }
-        this.width=50
-        this.height=50
-    }
-    draw() {
-        c.fillStyle="red"
-        c.fillRect(this.position.x,this.position.y,this.width,this.height)
-    }
-    update() {
-        this.draw()
-        this.position.x+=this.velocity.x
-        this.position.y+=this.velocity.y
-        if(this.position.y+this.height+this.velocity.y<=canvas.height){
-            this.velocity.y+=gravity
-        } else this.velocity.y=0;
-    }
-}
-
-class Platform {
-    constructor(){
-        this.position = {
-            x:200,
-            y:100
-        }
-        this.width=200
-        this.height=20
-    }
-    draw() {
-        c.fillStyle="blue"
-        c.fillRect(this.position.x,this.position.y,this.width,this.height)
-    }
+window.addEventListener("load",function() {
+    const canvas=document.getElementById("canvas1");
+    const ctx= canvas.getContext("2d");
+    canvas.width=1500;
+    canvas.height=500;
     
-}
 
-const player = new Player()
-
-const platform =new Platform()
-
-const keys = {
-    right:{
-        pressed:false
-    },
-    left: {
-        pressed:false
+    class InputHandler {
+        constructor(game) {
+            this.game=game;
+            window.addEventListener("keydown",(e) =>{
+                if(((e.key==="ArrowUp")||(e.key ==="ArrowDown")) && this.game.keys.indexOf(e.key)===-1) {
+                    this.game.keys.push(e.key)
+                } else if (e.key===" ") {
+                    this.game.player.shootTop();
+                }
+                console.log(this.game.keys)
+            })
+            window.addEventListener("keyup",(e)=>{
+                if(this.game.keys.indexOf(e.key)>-1) {
+                    this.game.keys.splice(this.game.keys.indexOf(e.key),1);
+                }
+                console.log(this.game.keys);
+            })
+        }
     }
-}
 
-function animate() {
-    requestAnimationFrame(animate)
-    c.clearRect(0,0,canvas.width,canvas.height)
-    player.update()
-    platform.draw()
-    if(keys.right.pressed) {
-        player.velocity.x=2
-    } else if(keys.left.pressed){
-        player.velocity.x-=2
-    } else player.velocity.x=0
-
-    if (player.position.y+player.height<=platform.position.y && player.position.y+player.height+player.velocity.y>=platform.position.y && player.position.x+player.width>=platform.position.x && player.position.x<=platform.position.x+platform.width){
-        player.velocity.y=0
+    class Projectile {
+        constructor(game,x,y) {
+            this.game = game;
+            this.x=x;
+            this.y=y;
+            this.width=10;
+            this.height=3;
+            this.speed=3
+            this.markedForDeletion=false;
+        }
+        update() {
+            this.x+=this.speed;
+            if(this.x>this.game.width*0.8) this.markedForDeletion=true;
+        }
+        draw(context) {
+            context.fillStyle="yellow";
+            context.fillRect(this.x,this.y,this.width,this.height)
+        }
     }
-}
-animate()
 
+    class Particle {
 
-window.addEventListener("keydown",({keyCode})=>{
-    switch (keyCode) {
-        case 65: console.log("left")
-        keys.left.pressed=true
-        break
-        case 83: console.log("down")
-        break
-        case 68: console.log("right")
-        keys.right.pressed=true
-        break
-        case 87: console.log("up")
-        player.velocity.y-=10
-        break
+    }
+
+    class Player {
+        constructor(game) {
+            this.game=game
+            this.height=100
+            this.width=120
+            this.x=20;
+            this.y=100;
+            this.speedY=0;
+            this.maxSpeed=2;
+            this.projectiles=[];
+        }
+
+        update() {
+            if(this.game.keys.includes("ArrowDown")) this.speedY+=this.maxSpeed
+            else if (this.game.keys.includes("ArrowUp")) this.speedY-=this.maxSpeed
+            else this.speedY=0;
+            this.y+=this.speedY
+            this.projectiles.forEach(projectile => {
+                projectile.update();
+            })
+            this.projectiles =this.projectiles.filter(projectile =>!projectile.markedForDeletion)
+        }
+
+        draw(context) {
+            context.fillStyle="red"
+            context.fillRect(this.x, this.y, this.width, this.height)
+            this.projectiles.forEach(projectile => {
+                projectile.draw(context);
+            })
+        }
+        shootTop() {
+            if(this.game.ammo>0) {
+                this.projectiles.push(new Projectile(this.game,this.x+80,this.y+30));
+                console.log(this.projectiles);
+                this.game.ammo--;
+            }
+        }
+    }
+
+    class Enemy {
         
     }
-    console.log(keys.right.pressed);
-})
-window.addEventListener("keyup",({keyCode})=>{
-    switch (keyCode) {
-        case 65: console.log("left")
-        keys.left.pressed=false
-        break
-        case 83: console.log("down")
-        break
-        case 68: console.log("right")
-        keys.right.pressed=false
-        break
-        case 87: console.log("up")
-        player.velocity.y-=10
-        break
-        
+
+    class Layer {
+
     }
-    console.log((keys.right.pressed));
+
+    class Background {
+
+    }
+
+    class UI {
+        constructor(game) {
+            this.game=game;
+            this.fontSize=25;
+            this.fontFamily="Helvetica";
+            this.color="white";
+        }
+        draw(context) {
+            context.fillStyle=this.color;
+            for(let i=0; i<this.game.ammo;i++) {
+                context.fillRect(20+5*i,50,3,20);
+            }
+        }
+    }
+
+    class Game {
+        constructor(width,height) {
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+            this.input =new InputHandler(this);
+            this.ui=new UI(this);
+            this.keys=[];
+            this.ammo=20;
+            this.maxAmmo=50;
+            this.ammoTimer=0;
+            this.ammoInterval=500;
+        }
+        update(deltaTime) {
+            this.player.update();
+            if(this.ammoTimer>this.ammoInterval) {
+                if(this.ammo<this.maxAmmo) this.ammo++;
+                this.ammoTimer=0;
+            } else {
+                this.ammoTimer+=deltaTime;
+            }
+        }
+
+        draw(context) {
+            this.player.draw(context);
+            this.ui.draw(context)
+        }
+    }
+    const game =new Game(canvas.width,canvas.height);
+    let lastTime=0;
+    function animate(timeStamp) {
+        const deltaTime=timeStamp-lastTime;
+        // console.log(deltaTime);
+        lastTime=timeStamp;
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        game.update(deltaTime);
+        game.draw(ctx)
+        requestAnimationFrame(animate)
+    }
+    animate(0);
 })
